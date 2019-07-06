8ce9f674f8eede994043e56a5132e7bb4f3fce49
Paris Cornett <pariscornett@gmail.com>
2019-07-05 22:28:59 -0500

    added spotify keys

--- javascript/keys.js
+++ javascript/keys.js
@@ -1,6 +1,6 @@
 console.log("this is loaded");
 
 exports.spotify= {
-    id: process.env.SPOTIFY_ID, //replace with personal keys
-    secret: process.env.SPOTIFY_SECRET
+    id: process.env.b3b2887fefce4f5ca159c852e25e2886, //replace with personal keys
+    secret: process.env.566a9e14d1c34aab8eda4523db8bc5cb
 };
