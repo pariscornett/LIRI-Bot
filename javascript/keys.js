1248dbcb55844aec32f728a4bef8293c132d5acf
Paris Cornett <pariscornett@gmail.com>
2019-07-06 21:20:13 -0500

    took away id and secret from keys.js

--- javascript/keys.js
+++ javascript/keys.js
@@ -1,6 +1,7 @@
 console.log("this is loaded");
-
+var spotifySecret = "566a9e14d1c34aab8eda4523db8bc5cb";
+var spotifyId = "b3b2887fefce4f5ca159c852e25e2886";
 exports.spotify= {
-    id: process.env.b3b2887fefce4f5ca159c852e25e2886, //replace with personal keys
-    secret: process.env.566a9e14d1c34aab8eda4523db8bc5cb
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
 };
