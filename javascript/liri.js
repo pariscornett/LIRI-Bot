66a3e21f3c7b65504428ff3926f883ce00f70221
Paris Cornett <pariscornett@gmail.com>
2019-07-06 21:30:35 -0500

    set up variables to store commands and user input seamlessly :fire:

--- javascript/liri.js
+++ javascript/liri.js
@@ -1,3 +1,12 @@
+//require all packages necessary to run app
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new spotify(keys.spotify);
+//var spotify = new spotify(keys.spotify);
+var axios = require("axios");
+var fs = require("fs");
+var moment = require("moment");
+var spotifyRequire = require("node-spotify-api");
+
+//global variables
+var command = process.argv[2] //stores the command
+var input = process.argv.slice(3).join(" "); //stores user input and avoids the need for camelCase, quotes, etc.
